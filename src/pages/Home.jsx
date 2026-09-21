c0a10919ce21692515e27cc6a71911b5c632c08f
Guilherme Figueiras <guilherme.d.figueiras@gmail.com>
2026-09-21 15:16:23 +0100

    Public homepage with services from Supabase.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,8 +36,8 @@ function Home() {
 
       <h2>Galeria</h2>
       <h2>Galeria</h2>
-<div style={{ display: 'flex', gap: '10px' }}>
-  <img src="/gallery/img1.jpg" width="150" />
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <img src="/gallery/img1.jpg" width="150" />
   <img src="/gallery/img2.jpg" width="150" />
 </div>
     </div>
